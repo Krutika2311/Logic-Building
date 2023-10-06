4e83d72159bd313c1281a26f330e803366aee7cc
Krutika2311 <krutikadalvi23@gmail.com>
2023-10-06 20:41:26 +0530

    My Commit

---
+++ Assignment 2/Assignment2a5.c
@@ -0,0 +1,41 @@
+// Problem Statement : Accept number from user and check whether number is even or odd
+
+#include<stdio.h>
+
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkEven(int iNo)
+{
+    if((iNo % 2) == 0)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    bRet = ChkEven(iValue);
+    
+    if(bRet == TRUE)
+    {
+        printf("%d is even number\n",iValue);
+    }
+    else
+    {
+        printf("%d is odd number\n",iValue);
+    }
+
+    return 0;
+}
